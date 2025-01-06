e3a8ef4e6b006da09a2581a5da806b76151c663b
mbortg995 <mbortg995@gmail.com>
2025-01-06 09:51:07 +0100

    Feat: Project Init + Basic modules setup.

---
+++ constants.configs.js
@@ -0,0 +1,5 @@
+
+export default {
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
+};
---
+++ database.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import keys from './constants.configs.js';
+
+const { MONGO_URI } = keys;
+
+const MONGO_DB_NAME = 'budgy_backend';
+const connectionConfig = { dbName: MONGO_DB_NAME };
+
+export async function connectToDatabase(){
+  try{
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(MONGO_URI, connectionConfig);
+    console.log('Connected to MongoDB');
+  } catch (error){
+    console.error('Error connecting to MongoDB: ', error);
+  }
+}
